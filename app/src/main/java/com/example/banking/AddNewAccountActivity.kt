6cbc9c1c7fd0249d0feb2e3c087bab787e7e6d19
Pravesh Choudhary <64903405+pravesh00@users.noreply.github.com>
2023-01-10 02:11:44 +0530

    New account XML

---
+++ app/src/main/java/com/example/banking/AddNewAccountActivity.kt
@@ -0,0 +1,11 @@
+package com.example.banking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNewAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_account)
+    }
+}
